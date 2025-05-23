0dd74d0cca8f023d2f8ba10a7e1b5a87ddfcc815
muryuryu <ToshiBruh>
2025-05-23 17:04:53 +0200

    added schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,11 +1,58 @@
-DROP SCHEMA IF EXISTS testing CASCADE;
-CREATE SCHEMA testing;
+-- DROP SCHEMA IF EXISTS slime_db CASCADE;
+CREATE SCHEMA slime_db;
 
-CREATE TABLE testing.users (
+CREATE TABLE slime_db.users (
 	id  BIGSERIAL PRIMARY KEY,
 	email       VARCHAR(200) NOT NULL,
 	first_name  VARCHAR(200) NOT NULL,
 	last_name   VARCHAR(200) NOT NULL,
 	username    VARCHAR(50) UNIQUE NOT NULL,
+	password    VARCHAR(200) NOT NULL,
 	UNIQUE (username)
 );
+
+CREATE TABLE slime_db.companies (
+	id  		BIGSERIAL PRIMARY KEY,
+	org_id	  	BIGINT NOT NULL,
+	name        VARCHAR(200) NOT NULL,
+	website     VARCHAR(200) NOT NULL,
+	phone       VARCHAR(50) NOT NULL,
+	email       VARCHAR(200) NOT NULL,
+	address     VARCHAR(200) NOT NULL,
+	city        VARCHAR(200) NOT NULL,
+	state       VARCHAR(50) NOT NULL,
+	zip         VARCHAR(20) NOT NULL,
+	country     VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE slime_db.opportunities (
+	id  		BIGSERIAL PRIMARY KEY,
+	org_id	  	BIGINT NOT NULL,
+	name        VARCHAR(200) NOT NULL,
+	commitee_id   BIGINT NOT NULL,
+	company_id  BIGINT NOT NULL,
+	created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE slime_db.commitee (
+	id  		BIGSERIAL PRIMARY KEY,
+	name        VARCHAR(200) NOT NULL,
+	created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE slime_db.people (
+	id  		BIGSERIAL PRIMARY KEY,
+	org_id	  	BIGINT NOT NULL,
+	first_name  VARCHAR(200) NOT NULL,
+	last_name   VARCHAR(200) NOT NULL,
+	email       VARCHAR(200) NOT NULL,
+	phone       VARCHAR(50) NOT NULL,
+	address     VARCHAR(200) NOT NULL,
+	city        VARCHAR(200) NOT NULL,
+	state       VARCHAR(50) NOT NULL,
+	zip         VARCHAR(20) NOT NULL,
+	country     VARCHAR(50) NOT NULL
+)
+
