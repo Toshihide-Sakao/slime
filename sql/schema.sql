2a408cc475352f23588fce847b39e059cb1758b3
muryuryu <ToshiBruh>
2025-05-23 17:14:35 +0200

    bruh

--- sql/schema.sql
+++ sql/schema.sql
@@ -27,12 +27,12 @@ CREATE TABLE slime_db.companies (
 
 CREATE TABLE slime_db.opportunities (
 	id  		BIGSERIAL PRIMARY KEY,
-	org_id	  	BIGINT NOT NULL,
 	name        VARCHAR(200) NOT NULL,
 	commitee_id   BIGINT NOT NULL,
 	company_id  BIGINT NOT NULL,
 	created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
 );
 
 CREATE TABLE slime_db.commitee (
@@ -54,5 +54,30 @@ CREATE TABLE slime_db.people (
 	state       VARCHAR(50) NOT NULL,
 	zip         VARCHAR(20) NOT NULL,
 	country     VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE slime_db.invoices (
+	id  		BIGSERIAL PRIMARY KEY,
+	company_id  BIGINT NOT NULL,
+	amount      DECIMAL(10, 2) NOT NULL,
+	due_date    DATE NOT NULL,
+	paid        BOOLEAN DEFAULT FALSE,
+	attachment  VARCHAR(200) NOT NULL,
+	created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE slime_db.products (
+	id  		BIGSERIAL PRIMARY KEY,
+	name        VARCHAR(200) NOT NULL,
+	description VARCHAR(200) NOT NULL,
+	price       DECIMAL(10, 2) NOT NULL,
+	created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE slime_db.notes (
+	id  		BIGSERIAL PRIMARY KEY,
+	opportunity_id BIGINT NOT
 )
 
