7a3e15a7cdd3fc6970c8fbfe72586f6d25732a7f
muryuryu <ToshiBruh>
2025-05-26 10:43:51 +0200

    orkar typ ej

--- src/database.rs
+++ src/database.rs
@@ -3,20 +3,18 @@ use crate::models::User;
 
 #[cfg(feature = "ssr")]
 pub async fn get_users(client: &Client) -> Result<Vec<User>, Error> {
-    use deadpool_postgres::{tokio_postgres::Error, Client};
-    use tokio_pg_mapper::FromTokioPostgresRow;
 
 
-    let stmt = include_str!("../sql/get_users.sql");
-    let stmt = stmt.replace("$table_fields", &User::sql_table_fields());
-    let stmt = client.prepare(&stmt).await.unwrap();
+    // let stmt = include_str!("../sql/get_users.sql");
+    // let stmt = stmt.replace("$table_fields", &User::sql_table_fields());
+    // let stmt = client.prepare(&stmt).await.unwrap();
 
-    let results = client
-        .query(&stmt, &[])
-        .await?
-        .iter()
-        .map(|row| User::from_row_ref(row).unwrap())
-        .collect::<Vec<User>>();
+    // let results = client
+    //     .query(&stmt, &[])
+    //     .await?
+    //     .iter()
+    //     .map(|row| User::from_row_ref(row).unwrap())
+    //     .collect::<Vec<User>>();
 
-    Ok(results)
+    // Ok(results)
 }
