60833ea8164ce9ca3f85dd16f1533f29ce5a1ae4
muryuryu <ToshiBruh>
2025-05-26 10:38:45 +0200

    ok

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,87 +1,116 @@
 -- DROP SCHEMA IF EXISTS slime_db CASCADE;
-CREATE SCHEMA slime_db;
 
-CREATE TABLE slime_db.users (
-	id  BIGSERIAL PRIMARY KEY,
+CREATE TABLE users (
+	id  		BIGSERIAL PRIMARY KEY,
+	kth_id    VARCHAR(50) UNIQUE NOT NULL,
 	email       VARCHAR(200) NOT NULL,
 	first_name  VARCHAR(200) NOT NULL,
 	last_name   VARCHAR(200) NOT NULL,
-	username    VARCHAR(50) UNIQUE NOT NULL,
 	password    VARCHAR(200) NOT NULL,
-	UNIQUE (username)
+	UNIQUE (kth_id)
 );
 
-CREATE TABLE slime_db.companies (
-	id  		BIGSERIAL PRIMARY KEY,
-	org_id	  	BIGINT NOT NULL,
+CREATE TABLE companies (
+	org_id	  	BIGINT PRIMARY KEY,
 	name        VARCHAR(200) NOT NULL,
-	website     VARCHAR(200) NOT NULL,
 	address     VARCHAR(200),
-	city        VARCHAR(200),
-	state       VARCHAR(50),
-	zip         VARCHAR(20),
-	country     VARCHAR(50) NOT NULL
-)
-
-CREATE TABLE slime_db.company_people (
-	company_id  BIGINT NOT NULL,
-	person_id   BIGINT NOT NULL,
-
-	PRIMARY KEY (company_id, person_id)
-)
-
-CREATE TABLE slime_db.opportunities (
-	id  		BIGSERIAL PRIMARY KEY,
-	name        VARCHAR(200) NOT NULL,
-	commitee_id   BIGINT NOT NULL,
-	company_id  BIGINT NOT NULL,
-	created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	invoice_adress VARCHAR(200)
 );
 
-CREATE TABLE slime_db.commitee (
-	id  		BIGSERIAL PRIMARY KEY,
-	name        VARCHAR(200) NOT NULL,
-	created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE slime_db.people (
+CREATE TABLE people (
 	id  		BIGSERIAL PRIMARY KEY,
 	org_id	  	BIGINT NOT NULL,
 	first_name  VARCHAR(200) NOT NULL,
 	last_name   VARCHAR(200) NOT NULL,
-	email       VARCHAR(200) NOT NULL,
-	phone       VARCHAR(50) NOT NULL,
-	address     VARCHAR(200) NOT NULL,
-	city        VARCHAR(200) NOT NULL,
-	state       VARCHAR(50) NOT NULL,
-	zip         VARCHAR(20) NOT NULL,
-	country     VARCHAR(50) NOT NULL
-)
+	email       VARCHAR(200),
+	phone       VARCHAR(50),
+	linkedin    VARCHAR(200),
+	notes	    TEXT,
+	created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
 
-CREATE TABLE slime_db.invoices (
+CREATE TABLE company_people (
+	org_id  	BIGINT NOT NULL REFERENCES companies(org_id) ON DELETE CASCADE,
+	person_id   BIGINT NOT NULL REFERENCES people(id) ON DELETE CASCADE,
+	
+	PRIMARY KEY (org_id, person_id)
+);
+
+CREATE TABLE opportunities (
 	id  		BIGSERIAL PRIMARY KEY,
-	company_id  BIGINT NOT NULL,
+	name        VARCHAR(200) NOT NULL,
+	created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE opportunity_people (
+	opportunity_id BIGINT NOT NULL REFERENCES opportunities(id) ON DELETE CASCADE,
+	person_id      BIGINT NOT NULL REFERENCES people(id) ON DELETE CASCADE,
+
+	PRIMARY KEY (opportunity_id, person_id)
+);
+
+CREATE TABLE opportunity_products (
+	opportunity_id BIGINT NOT NULL REFERENCES opportunities(id) ON DELETE CASCADE,
+	product_id     BIGINT NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+
+	PRIMARY KEY (opportunity_id, product_id)
+);
+
+CREATE TABLE opportunity_invoices (
+	opportunity_id BIGINT NOT NULL REFERENCES opportunities(id) ON DELETE CASCADE,
+	invoice_id     BIGINT NOT NULL REFERENCES invoices(id) ON DELETE CASCADE,
+
+	PRIMARY KEY (opportunity_id, invoice_id)
+);
+
+CREATE TABLE opportunity_committees (
+	opportunity_id BIGINT NOT NULL REFERENCES opportunities(id) ON DELETE CASCADE,
+	committee_id   BIGINT NOT NULL REFERENCES commitee(id) ON DELETE CASCADE,
+
+	PRIMARY KEY (opportunity_id, committee_id)
+);
+
+CREATE TABLE opportunity_user (
+	opportunity_id 	BIGINT NOT NULL REFERENCES opportunities(id) ON DELETE CASCADE,
+	u_id		    BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+
+	PRIMARY KEY (opportunity_id, u_id)
+);
+
+CREATE TABLE commitee (
+	id  		BIGSERIAL PRIMARY KEY,
+	name        VARCHAR(200) NOT NULL,
+);
+
+CREATE TABLE invoices (
+	id  		BIGSERIAL PRIMARY KEY,
+	org_id		BIGINT NOT NULL REFERENCES companies(org_id) ON DELETE CASCADE,
 	amount      DECIMAL(10, 2) NOT NULL,
 	due_date    DATE NOT NULL,
 	paid        BOOLEAN DEFAULT FALSE,
 	attachment  VARCHAR(200) NOT NULL,
+	notes       TEXT,
 	created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
-CREATE TABLE slime_db.products (
+CREATE TABLE products (
 	id  		BIGSERIAL PRIMARY KEY,
+	commitee_id   BIGINT NOT NULL REFERENCES commitee(id) ON DELETE CASCADE,
 	name        VARCHAR(200) NOT NULL,
 	description VARCHAR(200) NOT NULL,
 	price       DECIMAL(10, 2) NOT NULL,
 	created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
-CREATE TABLE slime_db.notes (
-	id  		BIGSERIAL PRIMARY KEY,
-	opportunity_id BIGINT NOT
-)
+CREATE TABLE notes (
+	id  			BIGSERIAL PRIMARY KEY,
+	opportunity_id  BIGINT NOT NULL REFERENCES opportunities(id) ON DELETE CASCADE,
+	content         TEXT NOT NULL,
+	created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
